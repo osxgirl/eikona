d3d184b33d8f0463674e066bbd04113b0dd38e9b
osxgirl <osxgirl@mac.com>
2021-06-23 11:21:45 -0400

    fetch serializer

---
+++ backend/app/controllers/messages_controller.rb
@@ -0,0 +1,42 @@
+class MessagesController < ApplicationController
+
+  def index
+    messages = Message.all
+    render json: MessageSerializer.new(messages)
+  end
+
+  def create
+    message = Message.new(messsage_params)
+         if message.save
+             render json: MessageSerializer.new(comment, include: [:user])
+         else
+             render json: {error: "could not save"}
+         end
+     end
+
+  def show
+    message = Message.find(params[:id])
+    render json: message.to_json(except: [:created_at, :updated_at], include: {user: {only: [:name]}})
+  end
+
+  def update
+      message = Message.find(params[:id])
+      if message.update(message_params)
+          render json: MessageSerializer.new(message)
+      else
+          render json: {error: "could not save"}
+      end
+  end
+
+  def destroy
+      message = Message.find(params[:id])
+      message.destroy
+      render json: {message: "successfully deleted #{message.detail}"}
+  end
+
+  private
+  def message_params
+      params.require(:message).permit(:message, :user_id)
+  end
+  
+end
