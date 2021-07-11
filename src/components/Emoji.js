75064008194dd4820a9098ab99d5eaf1141d9e7f
osxgirl <osxgirl@mac.com>
2021-07-10 20:45:12 -0400

    emoji removed

--- src/components/Emoji.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-import Emoji from "a11y-react-emoji";
-
-interface Props extends React.HTMLAttributes<HTMLSpanElement> {
-    label?: string; // optional
-    symbol: string; // required
-}
